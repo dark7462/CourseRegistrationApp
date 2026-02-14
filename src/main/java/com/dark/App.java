4539c39ae2daf9e767aef5bc0186b4992bf24ba3
dark7462 <anu870906@gmail.com>
2026-02-14 11:18:26 +0530

    UI done

--- src/main/java/com/dark/App.java
+++ src/main/java/com/dark/App.java
@@ -6,6 +6,21 @@ import javax.swing.*;
 
 public class App {
     public static void main(String[] args) {
+        run();
+    }
+
+    public static void run() {
+        try {
+            // Apply Nimbus Look and Feel for modern components
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Problem in loading LookAndFeel : " + e.getMessage());
+        }
         SwingUtilities.invokeLater(() -> new Main().setVisible(true));
     }
 }
--- src/main/java/com/dark/swingGUI/Main.java
+++ src/main/java/com/dark/swingGUI/Main.java
@@ -1,5 +1,6 @@
 package com.dark.swingGUI;
 
+import com.dark.App;
 import com.dark.entity.Course;
 import com.dark.entity.Student;
 import com.dark.entity.Teacher;
@@ -408,18 +409,6 @@ public class Main extends JFrame {
     }
 
     public static void main(String[] args) {
-        try {
-            // Apply Nimbus Look and Feel for modern components
-            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (Exception e) {
-            System.out.println("Problem in loading LookAndFeel : " + e.getMessage());
-        }
-
-        SwingUtilities.invokeLater(() -> new Main().setVisible(true));
+        App.run();
     }
 }
